d9fe2256c605e0e582fc530f815ac201ec80eaff
DonSutherland95 <58916345+DonSutherland95@users.noreply.github.com>
2020-04-07 12:55:01 -0400

    Updates

---
+++ precache-manifest.534e880ac60c5143fe61a8b371e470a3.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01da6b97c3e51d95d358a4661b092e95",
+    "url": "/hotel-inn/index.html"
+  },
+  {
+    "revision": "8d0b91af09e61a59e637",
+    "url": "/hotel-inn/static/css/main.1b3dbc8e.chunk.css"
+  },
+  {
+    "revision": "98462286695dc2814ffb",
+    "url": "/hotel-inn/static/js/2.77b764b8.chunk.js"
+  },
+  {
+    "revision": "8d0b91af09e61a59e637",
+    "url": "/hotel-inn/static/js/main.def3c166.chunk.js"
+  },
+  {
+    "revision": "b378fbca2a13e24facba",
+    "url": "/hotel-inn/static/js/runtime~main.5f42b92d.js"
+  },
+  {
+    "revision": "33ab9f1e88f204b5b8a13725a918f36c",
+    "url": "/hotel-inn/static/media/details-1.33ab9f1e.jpeg"
+  },
+  {
+    "revision": "50c23e6265e40d602f1ac080e2d4169a",
+    "url": "/hotel-inn/static/media/details-2.50c23e62.jpeg"
+  },
+  {
+    "revision": "7345b9f61680c799868f66f297cedab5",
+    "url": "/hotel-inn/static/media/details-3.7345b9f6.jpeg"
+  },
+  {
+    "revision": "7569a31606be3db6b229327eec5999df",
+    "url": "/hotel-inn/static/media/details-4.7569a316.jpeg"
+  },
+  {
+    "revision": "8f7c03bc4ec4621dec034198fb51ff15",
+    "url": "/hotel-inn/static/media/hotel-2.8f7c03bc.jpg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/hotel-inn/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/hotel-inn/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "3b5c08716be6b713154c8aedaec3f5bf",
+    "url": "/hotel-inn/static/media/room-10.3b5c0871.jpeg"
+  },
+  {
+    "revision": "f08fee37bdc8f3d11ef36658cfacc44e",
+    "url": "/hotel-inn/static/media/room-11.f08fee37.jpeg"
+  },
+  {
+    "revision": "5f069c4c1ad3d29cf3500920cbabce2c",
+    "url": "/hotel-inn/static/media/room-12.5f069c4c.jpeg"
+  },
+  {
+    "revision": "052c76815f889f20b8a498c19a212c50",
+    "url": "/hotel-inn/static/media/room-2.052c7681.jpeg"
+  },
+  {
+    "revision": "9a1103b597895b4c4b373d6304400781",
+    "url": "/hotel-inn/static/media/room-3.9a1103b5.jpeg"
+  },
+  {
+    "revision": "1e407d88a352a0e8493e2275a76f39f5",
+    "url": "/hotel-inn/static/media/room-4.1e407d88.jpeg"
+  },
+  {
+    "revision": "a5c06e0bb3e90f927159439d68b07253",
+    "url": "/hotel-inn/static/media/room-5.a5c06e0b.jpeg"
+  },
+  {
+    "revision": "b9a34e969ab86c1500d67183f646995a",
+    "url": "/hotel-inn/static/media/room-6.b9a34e96.jpeg"
+  },
+  {
+    "revision": "e251c8d571a7f753b3da985e4b8a34c6",
+    "url": "/hotel-inn/static/media/room-7.e251c8d5.jpeg"
+  },
+  {
+    "revision": "ebb7fca5f456fb07e0096ab3ef72b330",
+    "url": "/hotel-inn/static/media/room-8.ebb7fca5.jpeg"
+  },
+  {
+    "revision": "7b17c49f01aa17fc11df8400d604e1c9",
+    "url": "/hotel-inn/static/media/room-9.7b17c49f.jpeg"
+  },
+  {
+    "revision": "ee0d7dcd7a2c98174d126b4a5c4cb851",
+    "url": "/hotel-inn/static/media/suite.ee0d7dcd.png"
+  }
+]);
